031021f3b956878c674b817dfb4110235751e76b
Karamijit <karamjitverma9@gmail.com>
2022-02-25 16:50:20 +0530

    code formatter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,26 @@
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
-import {useState} from 'react';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+import { useState } from "react";
 
-function ExpenseItem(props){
-    //Array distructuring ==> order matters
-    //First one should describes the value and the second should update the value
-    const [title,setTitle]= useState(props.title);
+function ExpenseItem(props) {
+	//Array distructuring ==> order matters
+	//First one should describes the value and the second should update the value
+	const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle('Updated!');
-    }
+	const clickHandler = () => {
+		setTitle("Updated!");
+	};
 
-    return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
-    );
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+			<button onClick={clickHandler}>Change Title</button>
+		</Card>
+	);
 }
 export default ExpenseItem;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
-import './NewExpense.css';
-import ExpanseForm from './ExpenseForm';
+import "./NewExpense.css";
+import ExpanseForm from "./ExpenseForm";
 const NewExpense = () => {
 	return (
-    <div className='new-expense'>
-        <ExpanseForm/>
-    </div>);
+		<div className="new-expense">
+			<ExpanseForm />
+		</div>
+	);
 };
 export default NewExpense;
