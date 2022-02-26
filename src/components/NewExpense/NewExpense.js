1a254ed41a906914a0217e139366b144acb649d6
Karamijit <karamjitverma9@gmail.com>
2022-02-27 00:35:48 +0530

    Condition stmt for editing and stop editing

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,16 +1,34 @@
 import "./NewExpense.css";
 import ExpanseForm from "./ExpenseForm";
+import { useState } from "react";
 const NewExpense = (props) => {
-    const saveExpenseDataHandler=(enteredExpensedata)=>{
-        const entertdData={
-            ...enteredExpensedata,
-            id:Math.random().toString()
-        };
-        props.onAddExpanse(entertdData);
-    }
+    const [isEditing, setIsEditing] = useState(false);
+	const saveExpenseDataHandler = (enteredExpensedata) => {
+		const entertdData = {
+			...enteredExpensedata,
+			id: Math.random().toString(),
+		};
+		props.onAddExpanse(entertdData);
+        setIsEditing(false);
+	};
+    const startEditingHandler = () => {
+        setIsEditing(true);
+      };
+    
+      const stopEditingHandler = () => {
+        setIsEditing(false);
+      };
 	return (
 		<div className="new-expense">
-			<ExpanseForm onSaveExpenseData={saveExpenseDataHandler}/>
+			{!isEditing && (
+				<button onClick={startEditingHandler}>Add New Expense</button>
+			)}
+			{isEditing && (
+				<ExpanseForm
+					onSaveExpenseData={saveExpenseDataHandler}
+					onCancel={stopEditingHandler}
+				/>
+			)}
 		</div>
 	);
 };
