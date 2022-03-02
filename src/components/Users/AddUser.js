3987c77459bb1a86f057d19713c3d280ac65cdc9
Karamijit <karamjitverma9@gmail.com>
2022-03-03 00:23:52 +0530

    card component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from "../UI/Card";
+import classes from './AddUser.module.css'
 const AddUser = (props) => {
 	const addUser = (event) => {
 		event.preventDefault();
 	};
 	return (
-		<form onSubmit={addUser}>
-			{/* htmlFor is a prop name for assigning for attribute toa a label */}
-			<label htmlFor="username">User Name</label>
-			<input id="username" type="text" />
-			<label htmlFor="age">Age (years)</label>
-			<input id="age" type="number" />
-			<button type="submit">Add User</button>
-		</form>
+		<Card className={classes.input}>
+			<form onSubmit={addUser}>
+				{/* htmlFor is a prop name for assigning for attribute toa a label */}
+				<label htmlFor="username">User Name</label>
+				<input id="username" type="text" />
+				<label htmlFor="age">Age (years)</label>
+				<input id="age" type="number" />
+				<button type="submit">Add User</button>
+			</form>
+		</Card>
 	);
 };
 export default AddUser;
