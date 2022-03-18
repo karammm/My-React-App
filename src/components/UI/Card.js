dd11009e5cc88512e15e3e414e4136a7b30439e9
Karamijit <karamjitverma9@gmail.com>
2022-03-19 01:37:55 +0530

    comment

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import classes from "./Card.module.css";
 const Card = (props) => {
 	return (
+		// Incomming classes and own classess css
 		<div className={`${classes.card} ${props.className}`}>
 			{props.children}
 		</div>
