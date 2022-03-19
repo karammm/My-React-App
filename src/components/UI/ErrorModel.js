ee975b9be372d5ed9c7f5659921a1713eedd7538
Karamijit <karamjitverma9@gmail.com>
2022-03-19 14:44:45 +0530

    working with portals

--- src/components/UI/ErrorModel.js
+++ src/components/UI/ErrorModel.js
@@ -1,10 +1,15 @@
+import reactdom from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModel.module.css";
+import react from "react";
 const ErrorModel = (props) => {
-	return (
-		<div>
-			<div className={classes.backdrop} onClick={props.onConfirm} />
+	// So basically i split my model into two saparate component backdrop and model overlay as they will work in portals much easier
+	const Backdrop = (props) => {
+		return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+	};
+	const ModelOverlay = (props) => {
+		return (
 			<Card className={classes.modal}>
 				<header className={classes.header}>
 					<h2>{props.title}</h2>
@@ -16,7 +21,23 @@ const ErrorModel = (props) => {
 					<Button onClick={props.onConfirm}>Okay</Button>
 				</footer>
 			</Card>
-		</div>
+		);
+	};
+	return (
+		<>
+			{reactdom.createPortal(
+				<Backdrop onConfirm={props.onConfirm} />,
+				document.getElementById("backdrop-root")
+			)}
+			{reactdom.createPortal(
+				<ModelOverlay
+					title={props.title}
+					message={props.message}
+					onConfirm={props.onConfirm}
+				/>,
+				document.getElementById("overlay-root")
+			)}
+		</>
 	);
 };
 export default ErrorModel;
