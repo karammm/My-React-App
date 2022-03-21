283b2a08f1071d13e7b25498bd9152c62e15f339
Karamijit <karamjitverma9@gmail.com>
2022-03-22 01:03:48 +0530

    refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,45 +1,48 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModel from "../UI/ErrorModel";
 import classes from "./AddUser.module.css";
 const AddUser = (props) => {
-	const [enteredUserName, setEnteredUserName] = useState("");
-	const [enteredAge, setEnteredAge] = useState("");
-	const [error,setError]=useState();
+	// When a ref is passed to an element in render, a reference to the node becomes accessible at the current attribute of the ref.
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
+	const [error, setError] = useState();
 	const addUser = (event) => {
 		event.preventDefault();
-		if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+		const uname = nameInputRef.current.value;
+		const uage = ageInputRef.current.value;
+		if (uname.trim().length === 0 || uage.trim().length === 0) {
 			setError({
-				title:'Invalid Input',
-				message:'Please enter a valid name and age (non empty values).'
+				title: "Invalid Input",
+				message: "Please enter a valid name and age (non empty values).",
 			});
 			return;
-		} else if (+enteredAge < 1) {
+		} else if (+uage < 1) {
 			setError({
-				title:'Invalid Age',
-				message:'Please enter a valid age'
-			})
+				title: "Invalid Age",
+				message: "Please enter a valid age",
+			});
 			return;
 		}
-		props.onAddUser(enteredUserName, enteredAge);
-		setEnteredAge("");
-		setEnteredUserName("");
+		props.onAddUser(uname, uage);
+		nameInputRef.current.value = "";
+		ageInputRef.current.value = "";
 	};
-	const changeUserName = (event) => {
-		setEnteredUserName(event.target.value);
-	};
-	const ageChange = (event) => {
-		setEnteredAge(event.target.value);
-	};
-	const errorHandler=()=>{
+	const errorHandler = () => {
 		setError(null);
-	}
+	};
 	return (
 		<>
 			{/* If error is a thing and if it is thn error else using and and model if error would be undefinfed thn nothing will be rendered */}
-			{error && <ErrorModel title={error.title} message={error.message} onConfirm={errorHandler}/>}
+			{error && (
+				<ErrorModel
+					title={error.title}
+					message={error.message}
+					onConfirm={errorHandler}
+				/>
+			)}
 			<Card className={classes.input}>
 				<form onSubmit={addUser}>
 					{/* htmlFor is a prop name for assigning for attribute toa a label */}
@@ -47,17 +50,15 @@ const AddUser = (props) => {
 					<input
 						id="username"
 						type="text"
-						onChange={changeUserName}
-						value={enteredUserName}
 						placeholder="Please Enter User Name"
+						ref={nameInputRef}
 					/>
 					<label htmlFor="age">Age (years)</label>
 					<input
 						id="age"
 						type="number"
-						onChange={ageChange}
-						value={enteredAge}
 						placeholder="Please provide age"
+						ref={ageInputRef}
 					/>
 					<Button type="submit">Add User</Button>
 				</form>
