4396bf4635bd7af97c5798c786e453f77fc055ad
Karamijit <karamjitverma9@gmail.com>
2022-03-29 01:40:07 +0530

    Dividing into forward ref

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, useContext, useReducer } from "react";
+import React, {
+	useEffect,
+	useState,
+	useContext,
+	useReducer,
+	useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -34,6 +40,8 @@ const passwordReducer = (prevState, action) => {
 
 const Login = (pops) => {
 	const authCtx = useContext(AuthContext);
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 	// const [enteredEmail, setEnteredEmail] = useState("");
 	// const [emailIsValid, setEmailIsValid] = useState();
 	// const [enteredPassword, setEnteredPassword] = useState("");
@@ -85,13 +93,20 @@ const Login = (pops) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		authCtx.onLogin(emailState.value, passwordState.value);
+		if (formIsValid) {
+			authCtx.onLogin(emailState.value, passwordState.value);
+		} else if (!emailIsValid) {
+			emailInputRef.current.focus();
+		} else {
+			passwordInputRef.current.focus();
+		}
 	};
 
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
 				<Input
+					ref={emailInputRef}
 					id="email"
 					label="E-Mail"
 					type="email"
@@ -101,6 +116,7 @@ const Login = (pops) => {
 					onBlur={validateEmailHandler}
 				/>
 				<Input
+					ref={passwordInputRef}
 					id="password"
 					label="Password"
 					type="password"
@@ -110,7 +126,7 @@ const Login = (pops) => {
 					onBlur={validatePasswordHandler}
 				/>
 				<div className={classes.actions}>
-					<Button type="submit" className={classes.btn} disabled={!formIsValid}>
+					<Button type="submit" className={classes.btn}>
 						Login
 					</Button>
 				</div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,20 +1,38 @@
-import classes from './Input.module.css'
-const Input = (props) => {
+import React, { useRef, useEffect,useImperativeHandle } from "react";
+import classes from "./Input.module.css";
+const Input = React.forwardRef((props,ref) => {
+	const inputRef = useRef();
+
+	//TO focus the input after the component is rendered
+	// useEffect(() => {
+	//     inputRef.current.focus();
+	// }, []);
+
+	const activate = () => {
+		inputRef.current.focus();
+	};
+    useImperativeHandle(ref,()=>{
+        return {
+            focus:activate,
+        };
+    });
+
 	return (
-			<div
-				className={`${classes.control} ${
-					props.isValid === false ? classes.invalid : ""
-				}`}
-			>
-				<label htmlFor={props.id}>{props.label}</label>
-				<input
-					type={props.type}
-					id={props.id}
-					value={props.value}
-					onChange={props.onChange}
-					onBlur={props.onBlur}
-				/>
-			</div>
+		<div
+			className={`${classes.control} ${
+				props.isValid === false ? classes.invalid : ""
+			}`}
+		>
+			<label htmlFor={props.id}>{props.label}</label>
+			<input
+				ref={inputRef}
+				type={props.type}
+				id={props.id}
+				value={props.value}
+				onChange={props.onChange}
+				onBlur={props.onBlur}
+			/>
+		</div>
 	);
-};
+});
 export default Input;
