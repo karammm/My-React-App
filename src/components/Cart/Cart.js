8a1d14053eae4f0232e52e8916f4de619c5e7ed6
Karamijit <karamjitverma9@gmail.com>
2022-04-06 00:17:46 +0530

    shooping cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,24 @@
+import classes from "./Cart.module.css";
+const Cart = (props) => {
+	const cartItems = (
+		<ul className={classes["cart-item"]}>
+			{[{ id: "c1", name: "karam", amount: 2, price: 10.0 }].map((item) => (
+				<li>{item.name}</li>
+			))}
+		</ul>
+	);
+	return (
+		<div>
+			{cartitems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>35.63</span>
+			</div>
+			<div className={classes.actions}>
+				<button className={classes["button--alt"]}>Close</button>
+				<button className={classes.order}>Order</button>
+			</div>
+		</div>
+	);
+};
+export default Cart;
