65ba7252adea7581ec020080fb6161888b1444ea
Karamijit <karamjitverma9@gmail.com>
2022-04-09 00:08:00 +0530

    managing cart and model state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+	const [cartIsShowwn,setCartIsShown] = useState(false);
+
+	const showCartHandler = () =>{
+		setCartIsShown(true);
+	};
+
+	const hideCartHandler = () =>{
+		setCartIsShown(false);
+	};
+
 	return (
 		<Fragment>
-			<Cart/>
-			<Header />
+			{/* {}curly braces evaluates dynamic expressions */}
+			{cartIsShowwn && <Cart onClose={hideCartHandler}/>}
+			<Header onShowCart={showCartHandler}/>
 			<main>
 				<Meals />
 			</main>
