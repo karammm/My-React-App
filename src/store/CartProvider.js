57b97aad27b0aa8d39215b18975c3ba11016648d
Karamijit <karamjitverma9@gmail.com>
2022-04-17 14:18:21 +0530

    making items removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,25 +8,44 @@ const defaultCartState = {
 
 //outside of the component fun bcz this reducer function won't need anythong from that component
 const cartReducer = (state, action) => {
-		if (action.type === "ADD") {
+	if (action.type === "ADD") {
 		const existingCartItemIndex = state.items.findIndex(
 			(item) => item.id === action.item.id
 		);
-		const existingCartItem=state.items[existingCartItemIndex];
+		const existingCartItem = state.items[existingCartItemIndex];
 		let updatedItems;
-		if(existingCartItem){
-			const updatedItem={
+		if (existingCartItem) {
+			const updatedItem = {
 				...existingCartItem,
-				amount:existingCartItem.amount+action.item.amount
-			}
-			updatedItems=[...state.items];
-			updatedItems[existingCartItemIndex]=updatedItem;
-		}else{
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
 			//updatedItem={...action.item};
-			updatedItems=state.items.concat(action.item); //immutable way brand new array without edition the old array
+			updatedItems = state.items.concat(action.item); //immutable way brand new array without edition the old array
 		}
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action.type === "REMOVE") {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id != action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
