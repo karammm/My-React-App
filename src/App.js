04a545b61b8fa4b9d91f38c055293eaa6c2423a1
Karamijit <karamjitverma9@gmail.com>
2022-04-21 00:33:04 +0530

    async await

--- src/App.js
+++ src/App.js
@@ -5,25 +5,21 @@ import "./App.css";
 
 function App() {
 	const [movies, setMovies] = useState([]);
-	function fetchMovieHandler() {
+	async function fetchMovieHandler() {
 		//promise is an object which will eventually yield some data insted of immediately doing that bcz ofcourse
 		//sending a http request is an asyncronous task it dosn't finish immediately it can take a couple of seconds
 		//and therefore
-		fetch("https://swapi.dev/api/films/")
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movieData) => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title,
-						openingText: movieData.opening_crawl,
-						releaseDate: movieData.release_date,
-					};
-				});
-				setMovies(transformedMovies);
-			});
+		const response = await fetch("https://swapi.dev/api/films/");
+		const data = await response.json();
+		const transformedMovies = data.results.map((movieData) => {
+			return {
+				id: movieData.episode_id,
+				title: movieData.title,
+				openingText: movieData.opening_crawl,
+				releaseDate: movieData.release_date,
+			};
+		});
+		setMovies(transformedMovies);
 	}
 
 	return (
